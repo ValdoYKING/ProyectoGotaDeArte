69f23fe3de03a8b5e57394a52836ece7ec9dd50e
utn203241038 <203241038@alumnos.utn.edu.mx>
2023-09-28 01:47:34 -0600

    cambio_footer_y_artista

--- app/Views/biografia_Art/Artista.php
+++ app/Views/biografia_Art/Artista.php
@@ -5,26 +5,28 @@
 
 <div class="card bg-dark text-white">
     <img id="fondo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2aaNe-t8VZx5ApcfTKXzXA_FOk7oHlWO7hTWKdY-SmAfFRufOua5NLJxX6Bp-UR7uIik&usqp=CAU" class="card-img" alt="...">
-    <div class="card-img-overlay">
-        <div>
+    <div class="card-img-overlay row">
+        <div class="col-3 ">
             <img id="perfil" class="card-img card-img-left" src="https://enciclopedia.net/wp-content/uploads/2015/02/Artista.jpg" alt="Card image" />
-            <small class="text-muted"><?php echo $historia; ?></small></p>
-            <p class="card-text">
-            <button class=" btn btn-outline-dark" type="button">Seguir</button>
-
-        </div>
-        <div>
-        <h5 class="card-title"><?php echo $artista; ?></h5>
-        <h4 class="card-title"><?php echo $alias; ?></h4>
-        <h3 class="card-title"><?php echo $nacionalidad; ?></h3>
-        <p class="card-text"><?php echo $frase; ?></p>
-        <p class="card-text"><?php echo $fecha_n; ?></p>
-
+            
+                <h5 class="card-title"><?php echo $artista; ?></h5>
+                <h4 class="card-title"><?php echo $alias; ?></h4>
+                <h4 class="card-title"><?php echo $nacionalidad; ?></h4>
+                
+            </div>
+            <div class="col ">
+                
+                <h2 class="card-text"><?php echo $fecha_n; ?></h2>
+                <h6 class="card-text"><?php echo $historia; ?></h6>
+            
+                
+                <h4 class="card-text">"<?php echo $frase; ?>"</h4>
+                <button class="btn rounded-pill me-2 btn-dark" type="button">Seguir</button>
         </div>
     </div>
 </div>
-<div>
-    <h3>Obras del artista</h3>
+<div id="subtitulo">
+    <h3 >Obras del artista</h3>
 </div>
 
 <div class="row row-cols-1 row-cols-md-3 g-4">
@@ -83,7 +85,7 @@
         </div>
     </div>
 </div>
-
+<br>
 <?php echo $this->endSection(); ?>
 <?php echo $this->section('scripts'); ?>
 
