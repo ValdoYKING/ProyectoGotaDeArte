61caf7c4290ac2662298f15e0b7a2f7348fe2f1f
utn203241038 <203241038@alumnos.utn.edu.mx>
2023-10-21 21:55:24 -0600

    nose-que-hice-:c

--- app/Views/Principal/paginaInicial.php
+++ app/Views/Principal/paginaInicial.php
@@ -145,11 +145,11 @@
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <div class="around">
-                        <div class="card-text">
-                            <h4 class="">Quieres Seguir viendo más..?</h4>
-                            <h5>"Desatando el arte, inspirando el mundo."</h5></div>
-                        <div class="d-grid gap-2 col-6 mx-auto"> 
+                    <div id="te" class="around">
+                        <div id="te" class="card-text ">
+                            <h4 >Quieres Seguir viendo más..?</h4>
+                            <h5 >"Desatando el arte, inspirando el mundo."</h5></div>
+                        <div  class="d-grid gap-2 col-6 mx-auto"> 
                             <button class="btn btn-secondary btn-lg" type="button">REGISTRATE</button>
                         </div>
                     </div>
